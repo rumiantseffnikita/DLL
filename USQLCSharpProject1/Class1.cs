05498c40a78f2e027ca2e08dc2c6480418b781dd
румянцевнс <@105-07>
2025-03-24 14:45:08 +0300

    second commit

--- USQLCSharpProject1/Class1.cs
+++ USQLCSharpProject1/Class1.cs
@@ -1,15 +1,55 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Interfaces.Streaming;
-using Microsoft.Analytics.Types.Sql;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 
-namespace USQLCSharpProject1
+namespace ListLibrary
 {
-    public class Class1
+    public class CustomList<T>
     {
+        private List<T> _items = new List<T>();
+
+        // Добавление элемента
+        public void Add(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item), "Элемент не может быть null");
+
+            _items.Add(item);
+        }
+
+        // Удаление элемента
+        public bool Remove(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item), "Элемент не может быть null");
+
+            return _items.Remove(item);
+        }
+
+        // Поиск элемента
+        public bool Contains(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item), "Элемент не может быть null");
+
+            return _items.Contains(item);
+        }
+
+        // Получение элемента по индексу
+        public T Get(int index)
+        {
+            if (index < 0 || index >= _items.Count)
+                throw new ArgumentOutOfRangeException(nameof(index), "Индекс вне диапазона");
+
+            return _items[index];
+        }
+
+        // Очистка списка
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        // Количество элементов
+        public int Count => _items.Count;
     }
 }
